4f34889bfc64de2e71ff7913f8588e631672be03
chasnew <chanuwas.asw@gmail.com>
2021-11-21 17:20:41 -0800

    added speakers and listeners (in progress)

--- efficient_hierarchy_model.py
+++ efficient_hierarchy_model.py
@@ -3,17 +3,34 @@
 import numpy as np
 import pandas as pd
 from mesa import Agent, Model
+from mesa.time import RandomActivation
 
 class OpinionAgent(Agent):
-    """An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model):
+    """An agent with initial opinion"""
+    def __init__(self, unique_id, opinion, model):
         super().__init__(unique_id, model)
-        self.opinion = 1
+        self.opinion = opinion
+
+    def step(self):
+        if self.role == "speaker":
+            listeners = np.random.choice(self.schedule.agents, self.lim_listeners, replace=False)
+            for listener in listeners:
+                opinion_diff = self.opinion - listener.opinion
+                listener.opinion = listener.opinion + (0.01*opinion_diff)
 
 class OpinionFormationModel(Model):
-    """A model with some number of agents."""
-    def __init__(self, N):
+    """A model with some number of agents"""
+    def __init__(self, N, lim_listeners):
         self.num_agents = N
+        self.lim_listeners = lim_listeners
+        self.schedule = RandomActivation(self)
+
         # Create agents
         for i in range(self.num_agents):
-            a = OpinionAgent(i, self)
+            opinion = np.random.uniform(0, 1, size=1)
+            a = OpinionAgent(i, opinion, self)
+            self.schedule.add(a)
+
+    def step(self):
+        speaker = np.random.choice(self.schedule.agents, size=1)
+        self.schedule.step()

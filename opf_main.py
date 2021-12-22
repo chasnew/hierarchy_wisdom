789ad76ab7c33a932a473ce51bf33336a780adaf
chasnew <chanuwas.asw@gmail.com>
2021-12-21 21:26:38 -0800

    Prepared for a batch run

--- opf_main.py
+++ opf_main.py
@@ -2,40 +2,35 @@ import os
 from efficient_hierarchy_model import OpinionModel
 from mesa.batchrunner import BatchRunner
 
-# group_size_list = list(range(100, 401, 50))
-# n_sim = 100
-# nlead_list = list(range(51))
-# x_threshold = 0.05
-# k = 4
-# lead_alpha = 0.75
-# follw_alpha = 0.25
-# lim_listeners = 30
-variable_params = {'N': range(50, 101, 50)}
+box_path = '/Users/chanuwasaswamenakul/Library/CloudStorage/Box-Box'
+
+# need group size from 50 to 1000 (50 incremental step)
+variable_params = {'N': range(50, 251, 50),
+                   'nlead': range(51)}
 fixed_params = {'x_threshold': 0.05,
                 'k': 4,
-                'nlead': 0,
                 'lead_alpha': 0.75,
                 'follw_alpha': 0.25,
-                'lim_listeners': 20}
-n_sim = 3 # number of simulations per parameter combination
+                'lim_listeners': 30}
+n_sim = 100 # number of simulations per parameter combination
 
 batch_run = BatchRunner(OpinionModel,
                         variable_parameters=variable_params,
                         fixed_parameters=fixed_params,
                         iterations=n_sim,
-                        max_steps=1000,
+                        max_steps=10000,
                         model_reporters={'mean_opinion': lambda m: m.mean_opinion(),
                                          'sd_opinion': lambda m: m.sd_opinion(),
                                          'n_event': lambda m: m.n_event},
-                        agent_reporters={'opinion': 'opinion'}
+                        agent_reporters={'opinion': 'opinion',
+                                         'influence': 'alpha'}
                         )
 batch_run.run_all()
 
 # retrieve model-level results
 run_data = batch_run.get_model_vars_dataframe()
 # print(run_data.info())
-print(run_data['n_event'])
-run_data.to_csv(os.path.join('results', 'test.csv'), index=False)
+run_data.to_csv(os.path.join(box_path, 'HierarchyWisdom', 'results', 'test.csv'), index=False)
 
 # data_collector_agents = batch_run.get_collector_agents()
 # agent_data = data_collector_agents[(50,1)]

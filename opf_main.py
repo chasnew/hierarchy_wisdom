e957db351bd9bf7f1b81f6e9e45655f08d227ec6
chasnew <chanuwas.asw@gmail.com>
2022-01-06 22:17:15 -0800

    drop unimportant columns before saving the data

--- opf_main.py
+++ opf_main.py
@@ -14,17 +14,6 @@ fixed_params = {'x_threshold': 0.05,
                 'lim_listeners': 30}
 n_sim = 100 # number of simulations per parameter combination
 
-# batch_run = BatchRunner(OpinionModel,
-#                         variable_parameters=variable_params,
-#                         fixed_parameters=fixed_params,
-#                         iterations=n_sim,
-#                         max_steps=10000,
-#                         model_reporters={'mean_opinion': lambda m: m.mean_opinion(),
-#                                          'sd_opinion': lambda m: m.sd_opinion(),
-#                                          'n_event': lambda m: m.n_event},
-#                         agent_reporters={'opinion': 'opinion',
-#                                          'influence': 'alpha'}
-#                         )
 batch_run = BatchRunnerMP(OpinionModel,
                           nr_processes=None,
                           variable_parameters=variable_params,
@@ -41,8 +30,14 @@ batch_run.run_all()
 
 # retrieve model-level results
 run_data = batch_run.get_model_vars_dataframe()
+
+# drop unnecessary columns
+drop_cols = list(fixed_params.keys()) + ['Run']
+run_data.drop(columns=drop_cols, inplace=True)
+
 print(run_data.info())
 
+# save data
 result_file = os.path.join(box_path, 'HierarchyWisdom', 'results', 'consensus_results.csv')
 if os.path.exists(result_file):
     run_data.to_csv(result_file, index=False, header=False, mode='a')

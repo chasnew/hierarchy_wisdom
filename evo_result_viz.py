6954546abca06e9c78d89ee88038ed1df733b0d1
chasnew <chanuwas.asw@gmail.com>
2022-05-14 15:56:58 -0700

    fix scales of visualization

--- evo_result_viz.py
+++ evo_result_viz.py
@@ -7,8 +7,8 @@ import seaborn as sns
 plt.style.use('seaborn')
 
 box_path = '/Users/chanuwasaswamenakul/Library/CloudStorage/Box-Box'
-result_path = os.path.join(box_path, 'HierarchyWisdom', 'results', 'test_evo_results.csv')
-alpha_path = os.path.join(box_path, 'HierarchyWisdom', 'results', 'alpha_array.npy')
+result_path = os.path.join(box_path, 'HierarchyWisdom', 'results', 'replicated_evo_results.csv')
+alpha_path = os.path.join(box_path, 'HierarchyWisdom', 'results', 'replicated_pool_alpha.npy')
 
 result_df = pd.read_csv(result_path)
 result_df['group_id'] = result_df['group_id'].astype(str)
@@ -44,23 +44,41 @@ for alpha_row in cum_alpha:
 
 heatmap_array = np.array(heatmap_array)
 
+sns.set(rc={'figure.figsize': (15, 6)})
+
 # visualize alpha proportions
 new_yticks = list(range(0,251,25))
 ytick_labels = np.round(np.linspace(0,1,num=len(new_yticks)), 2)
 
-ax = sns.heatmap(heatmap_array.T, cmap='coolwarm')
+new_xticks = list(range(0,7501, 1500))
+# xtick_labels = np.linspace(0, 7500, num=len(new_xticks))
+
+ax = sns.heatmap(heatmap_array.T, cmap='coolwarm', cbar_kws={'shrink': 0.5,
+                                                             'label': 'influence'})
 ax.set_yticks(new_yticks)
 ax.set_yticklabels(ytick_labels)
-plt.title('pooled alpha distribution of 50 patches over time')
+ax.set_xticks(new_xticks)
+ax.set_xticklabels(new_xticks, rotation=0)
+# plt.title('pooled alpha distribution of 50 patches over time')
 plt.show()
 
-ax = sns.lineplot(x='step', y='group_size', hue='group_id', data=result_df)
+sample_groups = np.random.choice(np.arange(50), size=5, replace=True).astype(str)
+sample_results = result_df[result_df['group_id'].isin(sample_groups)]
+
+ax = sns.lineplot(x='step', y='group_size', hue='group_id',
+                  estimator=None, lw=1, data=sample_results)
 ax.get_legend().remove()
 plt.title('population size over time of different patches')
 plt.show()
 
-sns.lineplot(x='step', y='alpha_skewness', hue='group_id', data=result_df)
+sns.lineplot(x='step', y='group_size', data=result_df)
+plt.title('population size over time of different patches')
 plt.show()
 
-sns.lineplot(x='step', y='n_event', hue='group_id', data=result_df)
+sns.lineplot(x='step', y='alpha_skewness', data=result_df)
+plt.show()
+
+ax = sns.lineplot(x='step', y='n_event', hue='group_id',
+                  estimator=None, lw=1, data=result_df)
+ax.get_legend().remove()
 plt.show()

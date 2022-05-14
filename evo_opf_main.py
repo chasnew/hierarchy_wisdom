b8014593e464cdf7e374fe146b2562edd3ec739f
chasnew <chanuwas.asw@gmail.com>
2022-05-14 16:26:39 -0700

    pull parameters from yaml file

--- evo_opf_main.py
+++ evo_opf_main.py
@@ -1,57 +1,91 @@
 import os
+import numpy as np
 import pandas as pd
+import pickle
+import yaml
 from evo_hierarchy_model import EvoOpinionModel
 
-box_path = '/Users/chanuwasaswamenakul/Library/CloudStorage/Box-Box'
+with open('evo_opf_config.yaml') as file:
+    config_params = yaml.safe_load(file)
 
-# might try varying d and lim_listeners
-# variable_params = {'Ct': [2, 5]}
-fixed_params = {'init_n': 50,
-                'x_threshold': 0.05,
-                'k': 4,
-                'lim_listeners': 30,
-                'np': 1,
-                'mu': 0.01,
-                'mu_var': 0.01,
-                'K': 50,
-                'ra': 2,
-                'gammar': 0.025,
-                'betar': 3,
-                'gammab': 0.005,
-                'betab': 10000,
-                'S': 0.9,
-                'b_mid': 500,
-                'Ct': 2,
-                'd': 1}
+result_path = config_params['result_path']
+start_gen = config_params['start_gen']
+fixed_params = config_params['fixed_params']
 
-n_sim = 1 # number of simulations per parameter combination
-max_step = 5
+# load stored communities in case of on-going simulation
+pkl_path = os.path.join(result_path, 'replicated_communities.pkl')
 
+if start_gen > 1:
+    with open(pkl_path, 'rb') as file:
+        load_communities = pickle.load(file)
+
+    fixed_params['load_communities'] = load_communities
+else:
+    fixed_params['load_communities'] = None
+
+iter_num = config_params['iter_num']
+process_num = config_params['process_num']
+
+# initializing model
 evo_model = EvoOpinionModel(**fixed_params)
-for i in range(n_sim):
-    for j in range(max_step):
-        print('step', j)
-        evo_model.step(verbose=False)
+
+# prepare alpha-value collection
+alpha_pool = []
+
+alpha_sep_hists = []
+alpha_pool_hists = []
+
+if start_gen == 1:
+    # extract init alpha proportions
+    for c in evo_model.communities:
+        alpha_list = [agent.alpha for agent in c.population]
+        alpha_pool.extend(alpha_list)
+
+        alpha_hist = np.histogram(np.array(alpha_list), bins=50, range=(0, 1))[0]
+        alpha_sep_hists.append(alpha_hist)
+
+    alpha_hist = np.histogram(np.array(alpha_pool), bins=50, range=(0, 1))[0]
+    alpha_hist = alpha_hist/np.sum(alpha_hist)
+    alpha_pool_hists.append(alpha_hist)
+
+# evolutionary iterations
+for i in range(iter_num):
+    print('step', i)
+    evo_model.step(verbose=False, process_num=process_num)
+
+    # extract alpha proportions
+    alpha_pool = []
+
+    for c in evo_model.communities:
+        alpha_list = [agent.alpha for agent in c.population]
+        alpha_pool.extend(alpha_list)
+
+        alpha_hist = np.histogram(np.array(alpha_list), bins=50, range=(0, 1))[0]
+        alpha_hist = alpha_hist / np.sum(alpha_hist)
+        alpha_sep_hists.append(alpha_hist)
+
+    alpha_hist = np.histogram(np.array(alpha_pool), bins=50, range=(0, 1))[0]
+    alpha_hist = alpha_hist / np.sum(alpha_hist)
+    alpha_pool_hists.append(alpha_hist)
+
+filename = 'replicated_sep_alpha{}.npy'.format(start_gen)
+with open(os.path.join(result_path, filename), 'wb') as file:
+    np.save(file, np.array(alpha_sep_hists))
+
+filename = 'replicated_pool_alpha{}.npy'.format(start_gen)
+with open(os.path.join(result_path, filename), 'wb') as file:
+    np.save(file, np.array(alpha_pool_hists))
 
 # retrieve model-level results
-agent_data = pd.DataFrame(evo_model.datacollector)
-print(agent_data.info())
-print(agent_data.iloc[:, 1:7])
+community_data = pd.DataFrame(evo_model.datacollector)
+community_data['step'] = community_data['step'] + (start_gen-1)
+# print(community_data.info())
+# print(community_data.iloc[:5, :])
 
 # save data
-result_file = os.path.join(box_path, 'HierarchyWisdom', 'results', 'test_evo_results.csv')
-# if os.path.exists(result_file):
-#     agent_data.to_csv(result_file, index=False, header=False, mode='a')
-# else:
-agent_data.to_csv(result_file, index=False)
+filename = 'replicated_evo_results{}.csv'.format(start_gen)
+result_file = os.path.join(result_path, filename)
+community_data.to_csv(result_file, index=False)
 
-
-# scratch note
-
-# x_list = []
-# alpha_list = []
-#
-# for agent in c.population:
-#     x_list.append(agent.opinion)
-#     alpha_list.append(agent.alpha)
-
+# save communities
+evo_model.save_communities(filepath=pkl_path)

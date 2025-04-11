51d0f5efe39ba0d79da0c65ce83e5c826589e4c5
chasnew <chanuwas.asw@gmail.com>
2025-04-11 14:38:04 -0700

    Fitting beta distributions to distribution of influences in the population

--- inflc_dist_analysis.py
+++ inflc_dist_analysis.py
@@ -4,41 +4,53 @@ import pandas as pd
 import scipy
 
 sim = 1
-ct = -3
+ct = 3
 fixed_popsize = "all_groups"
-criterion = 'sd'
+criterion = 'prop'
+init_cond = 'most_followers'
 
-box_path = '/Users/chanuwasaswamenakul/Library/CloudStorage/Box-Box'
-result_path = os.path.join(box_path, 'HierarchyWisdom', 'results',
-                           'modevo_ct{}_{}_results_sim{}.csv'.format(ct, criterion, sim))
-alpha_path = os.path.join(box_path, 'HierarchyWisdom', 'results',
-                          'modpool_ct{}_{}_alpha_sim{}.npy'.format(ct, criterion, sim))
+for sim in range(1,6):
+    print('sim', sim)
 
-result_df = pd.read_csv(result_path)
-ng = result_df['group_id'].nunique()
+    box_path = '/Users/chanuwasaswamenakul/Library/CloudStorage/Box-Box'
+    result_path = os.path.join(box_path, 'HierarchyWisdom', 'results',
+                               'modevo_ct{}_{}_{}_results_sim{}.csv'.format(ct, criterion, init_cond, sim))
+    alpha_path = os.path.join(box_path, 'HierarchyWisdom', 'results',
+                              'modpool_ct{}_{}_{}_alpha_sim{}.npy'.format(ct, criterion, init_cond, sim))
 
-alpha_raw = np.load(alpha_path, allow_pickle=True)
+    result_df = pd.read_csv(result_path)
+    ng = result_df['group_id'].nunique()
 
-# fitting beta distribution
-beta_results = {'a': [], 'b': []}
-for i in range(alpha_raw.shape[0]):
-    a, b, loc, scale = scipy.stats.beta.fit(alpha_raw[i], floc=0, fscale=1.001)
-    beta_results['a'].append(a)
-    beta_results['b'].append(b)
+    alpha_raw = np.load(alpha_path, allow_pickle=True)
+    print('loaded alpha array.')
 
-beta_df = pd.DataFrame(beta_results)
-beta_df['step'] = list(range(alpha_raw.shape[0]))
+    # fitting beta distribution
+    beta_results = {'a': [], 'b': []}
+    for i in range(alpha_raw.shape[0]):
+        a, b, loc, scale = scipy.stats.beta.fit(alpha_raw[i], floc=0, fscale=1.000000001)
+        beta_results['a'].append(a)
+        beta_results['b'].append(b)
 
-beta_df.to_csv(os.path.join(box_path, 'HierarchyWisdom', 'results',
-                            'global_beta_ct{}_{}_sim{}.csv'.format(ct, criterion, sim)),
-               index=False)
+    print('complete fitting beta distribution.')
+
+    beta_df = pd.DataFrame(beta_results)
+    beta_df['step'] = list(range(alpha_raw.shape[0]))
+
+    beta_df.to_csv(os.path.join(box_path, 'HierarchyWisdom', 'results',
+                                'global_beta_ct{}_{}_{}_sim{}.csv'.format(ct, criterion, init_cond, sim)),
+                   index=False)
+    print('complete saving.')
+
+
 
 
 # alpha distributions for each separate group
 alphasep_path = os.path.join(box_path, 'HierarchyWisdom', 'results',
-                             'modsep_ct{}_{}_alpha_sim{}.npy'.format(ct, criterion, sim))
+                             'modsep_ct{}_{}_{}_alpha_sim{}.npy'.format(ct, criterion, init_cond, sim))
 
 concat_group_alpha = np.load(alphasep_path, allow_pickle=True)
+
+print('alpha array by groups')
 
 group_alpha_list = []
 
@@ -55,19 +67,21 @@ gbeta_results = {'step': [], 'group_id': [], 'a': [], 'b':[]}
 
 # fitting beta distribution to alpha for each group in each time step
 for i in range(ng):
+    print('group {}'.format(i))
+
     galpha_array = group_alpha_list[i]
     gbeta_results['step'].extend(step_list)
     gbeta_results['group_id'].extend([i] * step_num)
 
     for j in range(step_num):
-        a, b, loc, scale = scipy.stats.beta.fit(galpha_array[j], floc=0, fscale=1.001)
+        a, b, loc, scale = scipy.stats.beta.fit(galpha_array[j], floc=0, fscale=1.000000001)
         gbeta_results['a'].append(a)
         gbeta_results['b'].append(b)
-
-print('complete fitting beta distributions.')
 
 gbeta_df = pd.DataFrame(gbeta_results)
 
 gbeta_df.to_csv(os.path.join(box_path, 'HierarchyWisdom', 'results',
                              'group_beta_ct{}_{}_sim{}.csv'.format(ct, criterion, sim)),
                 index=False)
+
+print('complete processing and saving.')

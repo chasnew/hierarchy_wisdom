4f8dbefd66d57a2c28d14cd0da75446952935bdf
chasnew <chanuwas.asw@gmail.com>
2026-05-14 15:41:41 -0700

    commented out beta distribution fitting of influence distribution by group

--- inflc_dist_analysis.py
+++ inflc_dist_analysis.py
@@ -4,16 +4,16 @@ import pandas as pd
 import scipy
 import yaml
 
-sim = 1
+# sim = 11
 ct = 3
 fixed_popsize = "all_groups"
 criterion = 'prop'
-init_cond = 'most_followers'
+init_cond = 'uniform'
 
 with open('evo_opf_config.yaml') as file:
     config_params = yaml.safe_load(file)
 
-for sim in range(1,6):
+for sim in range(31,36):
     print('sim', sim)
 
     box_path = config_params['result_path']
@@ -46,41 +46,41 @@ for sim in range(1,6):
 
 
 # alpha distributions for each separate group
-alphasep_path = os.path.join(box_path, 'modsep_ct{}_{}_{}_alpha_sim{}.npy'.format(ct, criterion, init_cond, sim))
-
-concat_group_alpha = np.load(alphasep_path, allow_pickle=True)
-
-print('alpha array by groups')
-
-group_alpha_list = []
-
-# extract array of alpha of each group from the array
-for i in range(ng):
-    inds = list(range(i, concat_group_alpha.shape[0], ng))
-    group_alpha_list.append(concat_group_alpha[inds])
-
-step_num = group_alpha_list[0].shape[0]
-step_gap = int(alpha_raw.shape[0]/step_num)
-step_list = list(range(0, step_num, step_gap))
-
-gbeta_results = {'step': [], 'group_id': [], 'a': [], 'b':[]}
-
-# fitting beta distribution to alpha for each group in each time step
-for i in range(ng):
-    print('group {}'.format(i))
-
-    galpha_array = group_alpha_list[i]
-    gbeta_results['step'].extend(step_list)
-    gbeta_results['group_id'].extend([i] * step_num)
-
-    for j in range(step_num):
-        a, b, loc, scale = scipy.stats.beta.fit(galpha_array[j], floc=0, fscale=1.000000001)
-        gbeta_results['a'].append(a)
-        gbeta_results['b'].append(b)
-
-gbeta_df = pd.DataFrame(gbeta_results)
-
-gbeta_df.to_csv(os.path.join(box_path, 'group_beta_ct{}_{}_sim{}.csv'.format(ct, criterion, sim)),
-                index=False)
-
-print('complete processing and saving.')
+# alphasep_path = os.path.join(box_path, 'modsep_ct{}_{}_{}_alpha_sim{}.npy'.format(ct, criterion, init_cond, sim))
+#
+# concat_group_alpha = np.load(alphasep_path, allow_pickle=True)
+#
+# print('alpha array by groups')
+#
+# group_alpha_list = []
+#
+# # extract array of alpha of each group from the array
+# for i in range(ng):
+#     inds = list(range(i, concat_group_alpha.shape[0], ng))
+#     group_alpha_list.append(concat_group_alpha[inds])
+#
+# step_num = group_alpha_list[0].shape[0]
+# step_gap = int(alpha_raw.shape[0]/step_num)
+# step_list = list(range(0, step_num, step_gap))
+#
+# gbeta_results = {'step': [], 'group_id': [], 'a': [], 'b':[]}
+#
+# # fitting beta distribution to alpha for each group in each time step
+# for i in range(ng):
+#     print('group {}'.format(i))
+#
+#     galpha_array = group_alpha_list[i]
+#     gbeta_results['step'].extend(step_list)
+#     gbeta_results['group_id'].extend([i] * step_num)
+#
+#     for j in range(step_num):
+#         a, b, loc, scale = scipy.stats.beta.fit(galpha_array[j], floc=0, fscale=1.000000001)
+#         gbeta_results['a'].append(a)
+#         gbeta_results['b'].append(b)
+#
+# gbeta_df = pd.DataFrame(gbeta_results)
+#
+# gbeta_df.to_csv(os.path.join(box_path, 'group_beta_ct{}_{}_sim{}.csv'.format(ct, criterion, sim)),
+#                 index=False)
+#
+# print('complete processing and saving.')
